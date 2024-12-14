1f1946b3f448962d4dfa3d81442d23e1af437f0e
NESEGEMAA <nesegemaa@gmail.com>
2024-12-14 13:22:32 +0200

    Added a post request route in the router.js for the user's want-to-go post requests.

--- routes.js
+++ routes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const usercontroller = require("./controller/usercontroller");
 
 router.post("/signup", usercontroller.registration);
+router.post("/want-to-go", usercontroller.handleAddToWantToGo);
 
 module.exports = router;
